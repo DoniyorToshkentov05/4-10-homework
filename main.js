12ff90dbc8583ca69e1920e19a28f9c64ecf5629
Doniyortoshkentov05 <doniyortoshkentov@gmail.com>
2023-09-12 23:50:56 +0500

    end

---
+++ main.js
@@ -0,0 +1,164 @@
+// exam 1 exam 1 exam 1 exam 1 exam 1 exam 1 exam 1 exam 1 exam 1 exam 1 exam 1 exam 1 exam 1 exam 1 exam 1 exam 1
+
+// let time = new Date().getHours();
+
+// let fast_food = true;
+
+// if (time)  {
+// }
+// let order = ( time, work) =>{
+//     return new Promise((res, rej) => {
+//         if (fast_food){
+//             setTimeout(() => {
+//                 res(work())
+//             }, time);
+//         } else {
+//             rej (
+//                 console.log('error')
+//             )
+//         }
+//     })
+// }
+
+// order(2000, () => console.log('food tanlandi'))
+// .then( () => {
+//     return order(1000, () => console.log('tayyorlash boshlandi'))
+// })
+// .then( () => {
+//     return order(1000, () => console.log('tayyorlanmoqda'))
+// })
+// .then( () => {
+//     return order(1000, () => console.log('tayyor'))
+// })
+// .then( () => {
+//     return order(1000, () => console.log('mijozga topshirildi'))
+// })
+
+
+
+// exam 2 exam 2 exam 2 exam 2 exam 2 exam 2 exam 2 exam 2 exam 2 exam 2 exam 2 exam 2 exam 2 exam 2 exam 2 exam 2
+
+
+
+// let time = new Date().getHours();
+
+// let taxi = true;
+
+// if (time)  {
+// }
+// let order = ( time, work) =>{
+//     return new Promise((res, rej) => {
+//         if (taxi){
+//             setTimeout(() => {
+//                 res(work())
+//             }, time);
+//         } else {
+//             rej (
+//                 console.log('error')
+//             )
+//         }
+//     })
+// }
+
+// order(2000, () => console.log('logation aniqlandi'))
+// .then( () => {
+//     return order(1000, () => console.log('joy tanlandi'))
+// })
+// .then( () => {
+//     return order(1000, () => console.log('buyurtma berildi'))
+// })
+// .then( () => {
+//     return order(1000, () => console.log('buyurtma olindi'))
+// })
+// .then( () => {
+//     return order(1000, () => console.log('taxi jo`natildi'))
+// })
+// .then( () => {
+//     return order(1000, () => console.log('taxi yetib bordi'))
+// })
+// .then( () => {
+//     return order(1000, () => console.log('mijozga xitmat ko`rsatildi'))
+// })
+
+// exam 3 exam 3 exam 3 exam 3 exam 3 exam 3 exam 3 exam 3 exam 3 exam 3 exam 3 exam 3 exam 3 exam 3 exam 3 exam 3
+
+
+
+// let time = new Date().getHours();
+
+// let delaver = true;
+
+// if (time)  {
+// }
+// let order = ( time, work) =>{
+//     return new Promise((res, rej) => {
+//         if (delaver){
+//             setTimeout(() => {
+//                 res(work())
+//             }, time);
+//         } else {
+//             rej (
+//                 console.log('error')
+//             )
+//         }
+//     })
+// }
+
+// order(2000, () => console.log('logation aniqlandi'))
+// .then( () => {
+//     return order(1000, () => console.log('restorandan eat buyurtma berildi'))
+// })
+// .then( () => {
+//     return order(1000, () => console.log('yetqazib berish ishga tushdi'))
+// })
+// .then( () => {
+//     return order(1000, () => console.log('yetlazib beruvchi qabul qildi'))
+// })
+// .then( () => {
+//     return order(1000, () => console.log('restorandan eatni oldi'))
+// })
+// .then( () => {
+//     return order(1000, () => console.log('mijizga yetgazib berildi'))
+// })
+
+
+// exam 4 exam 4 exam 4 exam 4 exam 4 exam 4 exam 4 exam 4 exam 4 exam 4 exam 4 exam 4 exam 4 exam 4 exam 4 exam 4
+
+
+
+let time = new Date().getHours();
+
+let delaver = true;
+
+if (time)  {
+}
+let order = ( time, work) =>{
+    return new Promise((res, rej) => {
+        if (delaver){
+            setTimeout(() => {
+                res(work())
+            }, time);
+        } else {
+            rej (
+                console.log('error')
+            )
+        }
+    })
+}
+
+order(2000, () => console.log('logation aniqlandi'))
+.then( () => {
+    return order(1000, () => console.log('restorandan eat buyurtma berildi'))
+})
+.then( () => {
+    return order(1000, () => console.log('yetqazib berish ishga tushdi'))
+})
+.then( () => {
+    return order(1000, () => console.log('yetlazib beruvchi qabul qildi'))
+})
+.then( () => {
+    return order(1000, () => console.log('restorandan eatni oldi'))
+})
+.then( () => {
+    return order(1000, () => console.log('mijizga yetgazib berildi'))
+})
